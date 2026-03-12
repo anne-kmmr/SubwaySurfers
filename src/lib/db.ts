999509e32ba1536642c5b08842b7ee2609e60649
anne-kmmr <annminecraft07@gmx.de>
2026-03-12 14:35:37 +0100

    fix pipeline

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,7 @@
 import { neon } from "@neondatabase/serverless";
 
-// Verbindung zur Neon/Postgres-Datenbank über Environment Variable
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL ist nicht gesetzt!");
+}
+
 export const sql = neon(process.env.DATABASE_URL);
